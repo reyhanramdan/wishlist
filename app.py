a29bb7ce71c0bd55912ccc58bc239ca9f7398042
reyhanramdan <134348142+reyhanramdan@users.noreply.github.com>
2023-05-28 15:57:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from http import client
 from flask import Flask, render_template, request, jsonify, escape
 from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
 import os
 from os.path import join, dirname
@@ -18,40 +21,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form["bucket_give"]
-    count = db.bucket.count_documents({})
-    num = count + 1
+# route untuk memasukan data ke database
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
     doc = {
-        'num': num,
-        'bucket': escape(bucket_receive),
-        'done': 0
+        'name': name_receive,
+        'comment': comment_receive
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg':'data saved!'})
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg': 'data berhasil ditambahkan'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form["num_give"]
-    db.bucket.update_one(
-        {'num': int(num_receive)},
-        {'$set': {'done': 1}}
-    )
-    return jsonify({'msg': 'Update done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    data = list(db.bucket.find({}, {'_id':False}))
-    return jsonify({'buckets': data})
-
-@app.route("/bucket/del", methods=["POST"])
-def bucket_delete():
-    num_receive = int(request.form['num_give'])
-    db.bucket.delete_one({'num': num_receive})
-    return jsonify({'msg': 'data berhasil di hapus'})
+# route untuk mengambil data
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'message': message_list})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
