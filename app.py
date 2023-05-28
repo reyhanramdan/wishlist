3921d283b764b3a8cbb85331c2023828d894e269
reyhanramdan <134348142+reyhanramdan@users.noreply.github.com>
2023-05-28 16:02:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,5 @@
-from http import client
 from flask import Flask, render_template, request, jsonify, escape
 from pymongo import MongoClient
-import requests
-from bs4 import BeautifulSoup
-
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-DB_HOST = os.environ.get("MONGODB_URL")
-DB_NAME = os.environ.get("DB_NAME")
-
-client = MongoClient(DB_HOST)
-db = client[DB_NAME]
-from http import client
-from flask import Flask, render_template, request, jsonify, escape
-from pymongo import MongoClient
-import requests
-from bs4 import BeautifulSoup
 
 import os
 from os.path import join, dirname
@@ -39,51 +18,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-# route untuk memasukan data ke database
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form["bucket_give"]
+    count = db.bucket.count_documents({})
+    num = count + 1
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
+        'num': num,
+        'bucket': escape(bucket_receive),
+        'done': 0
     }
-    db.fanmessages.insert_one(doc)
-    return jsonify({'msg': 'data berhasil ditambahkan'})
+    db.bucket.insert_one(doc)
+    return jsonify({'msg':'data saved!'})
 
-# route untuk mengambil data
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {'_id': False}))
-    return jsonify({'message': message_list})
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one(
+        {'num': int(num_receive)},
+        {'$set': {'done': 1}}
+    )
+    return jsonify({'msg': 'Update done!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    data = list(db.bucket.find({}, {'_id':False}))
+    return jsonify({'buckets': data})
+
+@app.route("/bucket/del", methods=["POST"])
+def bucket_delete():
+    num_receive = int(request.form['num_give'])
+    db.bucket.delete_one({'num': num_receive})
+    return jsonify({'msg': 'data berhasil di hapus'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# route untuk memasukan data ke database
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {
-        'name': name_receive,
-        'comment': comment_receive
-    }
-    db.fanmessages.insert_one(doc)
-    return jsonify({'msg': 'data berhasil ditambahkan'})
-
-# route untuk mengambil data
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {'_id': False}))
-    return jsonify({'message': message_list})
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
